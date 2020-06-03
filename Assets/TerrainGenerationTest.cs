3a28bee0f0fff44c0f58cbaa7ccfcb051f9e098b
Christina Pagger <christina.pagger@t-matix.com>
2020-06-03 14:37:47 +0200

    Added simple terrain generation

---
+++ Assets/TerrainGenerationTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerationTest : MonoBehaviour
+{
+    TerrainPrefabs prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabs = this.GetComponent<TerrainPrefabs>();
+        Instantiate(prefabs.main_tile, new Vector3(0, 0, 0), Quaternion.identity);
+
+        // for(int i = 1; i < 5; i++){
+        //     for(int j = 0; j < 5; j++){
+        //         Instantiate(prefabs.tree_tile, new Vector3(i, 0, j), Quaternion.identity);
+        //     }
+        // }
+
+        // for(int i = -1; i > -5; i--){
+        //     for(int j = 0; j > -5; j--){
+        //         Instantiate(prefabs.tree_tile, new Vector3(i, 0, j), Quaternion.identity);
+        //     }
+        // }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TerrainPrefabs.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainPrefabs : MonoBehaviour
+{
+    public GameObject main_tile;
+    public GameObject tree_tile;
+}
